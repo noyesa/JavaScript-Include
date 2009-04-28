9625eef3c4106dfcd0235c7ed911c614daf8585d
Andrew Noyes <noyesa@gmail.com>
2009-04-28 14:48:59 -0400

    Added some helpful documentation

--- src/example.js
+++ src/example.js
@@ -1,11 +1,25 @@
 (function () {
+	/*
+		Create reference to namespace so that the
+		actual namespace only has to be referenced
+		once in the module. This way, if you want to
+		change the namespace later, you don't have to
+		F&R or go hunting through your code.
+	*/
 	var ns = namespace("com.andrew");
 	
+	// Private variable
+	var privVar = "Private!";
+	
+	// Private function
 	function priv() {
-		alert("Private!");
+		return privVar;
 	}
 	
+	ns.pubVar = "Public!";
+	
+	// Public function; accesses private function through closure
 	ns.pub = function () {
-		priv();
+		alert(priv() + "\n" + this.pubVar);
 	};
 })();
